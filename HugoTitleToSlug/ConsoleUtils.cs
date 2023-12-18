7a2b7dc98154ca87891fc707114abd473cc43c6e
SlimeNull <slimenull@outlook.com>
2023-12-18 15:29:50 +0800

    Add project files.

---
+++ HugoTitleToSlug/ConsoleUtils.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsdnArticleExtract.Utilities
+{
+    public static class ConsoleUtils
+    {
+        public static void PressAnyKeyToContinue(string prompt)
+        {
+            Console.Write($"[#] {prompt}");
+            Console.ReadKey(true);
+            Console.WriteLine();
+        }
+
+        public static void PressAnyKeyToContinue(bool noPrompt)
+        {
+            if (noPrompt)
+                Console.ReadKey(true);
+            else
+                PressAnyKeyToContinue("Press any key to continue.");
+        }
+
+        public static void PressAnyKeyToContinue()
+        {
+            PressAnyKeyToContinue(false);
+        }
+
+        public static string Input(string prompt)
+        {
+            Console.Write($"[#] {prompt} ");
+            string? input = Console.ReadLine();
+            if (input == null)
+                Environment.Exit(0);
+            return input!;
+        }
+
+        public static string InputUntil(string prompt, Func<string, bool> validator)
+        {
+            Console.Write($"[#] {prompt} ");
+
+            while (true)
+            {
+                string? input =
+                    Console.ReadLine();
+
+                if (input == null)
+                    Environment.Exit(0);
+
+                if (validator.Invoke(input))
+                    return input;
+
+                Console.WriteLine("[X] Invalid input!");
+            }
+        }
+
+        public static int Select(string prompt, params string[] options)
+        {
+            Console.WriteLine($"[#] {prompt}");
+            for (int i = 0; i < options.Length; i++)
+                Console.WriteLine($"  {i + 1}. {options[i]}");
+
+            while (true)
+            {
+                Console.Write("[#] Please select an option: ");
+
+                string? input = 
+                    Console.ReadLine();
+
+                if (input == null)
+                    Environment.Exit(0);
+
+                if (int.TryParse(input, out int result))
+                {
+                    if (result >= 1 && result <= options.Length)
+                        return result - 1;
+                }
+            }
+        }
+
+        public static TEnum Select<TEnum>(string prompt) where TEnum : struct, Enum
+        {
+            TEnum[] values = 
+                Enum.GetValues<TEnum>();
+
+            string[] options =
+                values.Select(value => value.ToString())
+                .ToArray();
+
+            return values[Select(prompt, options)];
+        }
+
+        public static bool YesOrNo(string prompt, bool? defaultValue = true)
+        {
+            string promptTail;
+            if (!defaultValue.HasValue)
+                promptTail = "(y/n)";
+            else if (defaultValue.Value)
+                promptTail = "(Y/n)";
+            else
+                promptTail = "(y/N)";
+
+            while (true)
+            {
+                Console.Write($"[?] {prompt} {promptTail}");
+
+                var keyInfo =
+                    Console.ReadKey();
+
+                if (keyInfo.Key == ConsoleKey.Enter && defaultValue.HasValue)
+                {
+                    Console.WriteLine();
+                    return defaultValue.Value;
+                }
+                else if (keyInfo.Key == ConsoleKey.Y)
+                {
+                    Console.WriteLine();
+                    return true;
+                }
+                else if (keyInfo.Key == ConsoleKey.N)
+                {
+                    Console.WriteLine();
+                    return false;
+                }
+            }
+        }
+
+
+        public static void Log(string message) =>
+            Console.WriteLine($"[#] {message}");
+
+        public static void Warn(string warning) => 
+            Console.WriteLine($"[!] {warning}");
+
+        public static void Error(string error) => 
+            Console.WriteLine($"[X] {error}");
+
+        public static void Tip(string tip) =>
+            Console.WriteLine($"[@] {tip}");
+    }
+}
---
+++ HugoTitleToSlug/Program.cs
@@ -0,0 +1,167 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+using CsdnArticleExtract.Utilities;
+
+namespace HugoTitleToSlug
+{
+    internal class Program
+    {
+        static Regex frontMatterRegex = new(@"^---(.|\n|\r)*?---");
+        static Regex frontMatterTitleRegex = new(@"^title: (?<content>.*)(\r|\n)+", RegexOptions.Multiline);
+        static Regex frontMatterSlugRegex = new(@"^slug: (?<content>.*)(\r|\n)+", RegexOptions.Multiline);
+
+        static void Main(string[] args)
+        {
+            string dir = ConsoleUtils.InputUntil("输入存储博客的目录: ", v => Directory.Exists(v));
+            string[] blogFiles = Directory.GetFiles(dir, "*.md", SearchOption.AllDirectories);
+
+            foreach (var blogFile in blogFiles)
+            {
+                string markdown = File.ReadAllText(blogFile);
+                string newMarkdown = InsertSlugToBlog(markdown, out string? generatedSlug);
+
+                if (markdown == newMarkdown)
+                {
+                    ConsoleUtils.Log($"No Change for: {Path.GetFileName(blogFile)}");
+                }
+                else
+                {
+                    if (generatedSlug is string)
+                    {
+                        File.WriteAllText(blogFile, newMarkdown);
+                        ConsoleUtils.Log($"Slug for {Path.GetFileName(blogFile)}: {generatedSlug}");
+                    }
+                    else
+                    {
+                        ConsoleUtils.Warn($"No Slug for {Path.GetFileName(blogFile)}");
+                    }
+                }
+            }
+        }
+
+        static string? GetSlugFromFrontMatter(string frontMatter)
+        {
+            if (frontMatterTitleRegex.Match(frontMatter) is Match match && match.Success)
+            {
+                string title = match.Groups["content"].Value;
+
+                title = title.Trim();
+                title = title.Trim('"');
+                title = title.Trim('\'');
+
+                return GetSlugFromTitle(title);
+            }
+
+            return null;
+        }
+
+        static string InsertSlugToFrontMatter(string frontMatter, out string? slug)
+        {
+            var frontMatterContent = frontMatter
+                .Trim('-')
+                .Trim();
+
+            slug = null;
+            if (GetSlugFromFrontMatter(frontMatterContent) is string generatedSlug)
+            {
+                bool replacedExist = false;
+                frontMatterContent = frontMatterSlugRegex.Replace(frontMatterContent, match =>
+                {
+                    replacedExist = true;
+                    return $"slug: '{generatedSlug}'{Environment.NewLine}";
+                });
+
+                if (!replacedExist)
+                {
+                    frontMatterContent = frontMatterTitleRegex.Replace(frontMatterContent, match => $"{match.Value.Trim()}{Environment.NewLine}slug: '{generatedSlug}'{Environment.NewLine}");
+                }
+
+                slug = generatedSlug;
+            }
+
+            return
+                $"""
+                ---
+                {frontMatterContent}
+                ---
+                """;
+        }
+
+        static string InsertSlugToBlog(string markdown, out string? slug)
+        {
+            string? generatedSlug = null;
+            string newBlogContent = frontMatterRegex.Replace(markdown, match =>
+            {
+                string newFrontMatter = InsertSlugToFrontMatter(match.Value, out string? generatedSlugInner);
+                if (generatedSlugInner is string)
+                    generatedSlug = generatedSlugInner;
+
+                return newFrontMatter;
+            });
+
+            slug = generatedSlug;
+            return newBlogContent;
+        }
+
+        static Dictionary<string, string> codingLanguages = new()
+        {
+            { "C#", "CSharp" },
+            { "c#", "CSharp" },
+
+            { "C++", "CPP" },
+            { "c++", "CPP" },
+
+            { ".NET", "DotNet" },
+            { ".net", "dotnet" }
+        };
+
+        static string ReplaceCodingLanguages(string text)
+        {
+            foreach (var lang in codingLanguages)
+            {
+                text = Regex.Replace(text, Regex.Escape(lang.Key), match =>
+                {
+                    int index = match.Index;
+                    int nextCharIndex = match.Index + match.Length;
+
+                    if (index != 0 && char.IsAsciiLetter(text[index - 1]))
+                        return match.Value;
+                    if (nextCharIndex < text.Length && char.IsAsciiLetter(text[nextCharIndex]))
+                        return match.Value;
+
+                    return lang.Value;
+                });
+            }
+
+            return text;
+        }
+
+        static string ReplaceSpecialChars(string text)
+        {
+            StringBuilder sb = new StringBuilder(text);
+            sb.Replace('\\', ',');
+            sb.Replace('/', ',');
+            sb.Replace(':', ' ');
+            sb.Replace('*', ' ');
+            sb.Replace('?', ',');
+            sb.Replace('!', ',');
+            sb.Replace('"', ' ');
+            sb.Replace('\'', ' ');
+            sb.Replace('<', '(');
+            sb.Replace('>', ')');
+            sb.Replace('|', ' ');
+
+            return sb.ToString();
+        }
+
+        static string GetSlugFromTitle(string title)
+        {
+            string slug = title;
+            slug = Regex.Replace(slug, @"\s", "");
+            slug = ReplaceCodingLanguages(slug);
+            slug = ReplaceSpecialChars(slug);
+
+            return slug;
+        }
+    }
+}
